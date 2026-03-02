a8166a2a3cc2919e2be8b9222e5d425d8928457d
angrypig555 <brny@brny.dev>
2026-03-02 18:13:19 +0000

    add tmp folder

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
+#include<fstream>
+#include<filesystem>
 
 std::string filename;
+std::filesystem::path tmp_dir = "/tmp/bread/";
+
+int create_tmp_dir_if_not_exist() {
+    try {
+        if (!std::filesystem::is_directory(tmp_dir)) {
+            std::filesystem::create_directory(tmp_dir);
+            std::cout << "created tmp directory, first start?" << std::endl;
+        }
+    } catch (const std::filesystem::filesystem_error &e) {
+        std::cout << "could not create tmp directory, " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -9,5 +25,6 @@ int main(int argc, char *argv[]) {
     } else {
         filename = argv[1];
     }
+    create_tmp_dir_if_not_exist();
     return 0;
 }
